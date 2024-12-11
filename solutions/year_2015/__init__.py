a27fecf9dd1cb2623522caa820ab50ca5934d159
Kyrylo <kyrylo.ktl@gmail.com>
2024-12-11 13:55:22 +0200

    Fixed import for year 2015 day 4 task solutions

--- solutions/year_2015/__init__.py
+++ solutions/year_2015/__init__.py
@@ -4,6 +4,8 @@ from .day_10.part_one import Year2015Day10Part1Solution
 from .day_10.part_two import Year2015Day10Part2Solution
 from .day_12.part_one import Year2015Day12Part1Solution
 from .day_12.part_two import Year2015Day12Part2Solution
+from .day_4.part_one import Year2015Day4Part1Solution
+from .day_4.part_two import Year2015Day4Part2Solution
 from .day_5.part_one import Year2015Day5Part1Solution
 from .day_5.part_two import Year2015Day5Part2Solution
 from .day_6.part_one import Year2015Day6Part1Solution
@@ -16,6 +18,9 @@ from .day_9.part_two import Year2015Day9Part2Solution
 __all__ = (
     'Year2015Day1Part1Solution',
     'Year2015Day1Part2Solution',
+
+    'Year2015Day4Part1Solution',
+    'Year2015Day4Part2Solution',
 
     'Year2015Day5Part1Solution',
     'Year2015Day5Part2Solution',
---
+++ solutions/year_2015/day_4/part_two.py
@@ -0,0 +1,38 @@
+from hashlib import md5
+from typing import override
+
+from infrastructure.solutions.base import Solution
+
+
+class Year2015Day4Part2Solution(Solution):
+    HASH_PREFIX = '000000'
+
+    @classmethod
+    @override
+    def parse_input(cls, text_input: str) -> dict[str, str]:
+        return {'secret_key': text_input}
+
+    @classmethod
+    @override
+    def solve(cls, secret_key: str) -> int:
+        """
+        Time:  O((n+m)*16^m)
+        Space: O(n+m)
+
+        Where n - length of secret key
+              m - length of pattern (5 in this case)
+        """
+        min_number = 0
+
+        while not cls.get_hash(secret_key, min_number).startswith(cls.HASH_PREFIX):
+            min_number += 1
+
+        return min_number
+
+    @staticmethod
+    def get_hash(secret_key: str, min_number: int) -> str:
+        return md5((secret_key + str(min_number)).encode()).hexdigest()
+
+
+if __name__ == '__main__':
+    print(Year2015Day4Part2Solution.main())
